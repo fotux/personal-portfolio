4eebbda2ff219663ec0b284923cd88adcd177348
Fotux <karooolys@gmail.com>
2025-07-25 15:45:18 +0300

    nav colors picking, emoji added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<main class="bg-background">
+    <nav class="bg-dark text-light py-4 text-lg list-none flex justify-around font-semibold ">
+        <div
+            class="font-sans font-bold text-2xl from-primary to-secondary bg-gradient-to-b text-transparent bg-clip-text relative tracking-widest">
+            Karolis Raginkis
+            <div class="absolute w-14 h-0.5 bg-gradient-to-t from-primary to-secondary rounded-full"></div>
+        </div>
+        </div>
+        <div class="flex gap-10 text-light tracking-tight ">
+            <li><span
+                    class="bg-gradient-to-t from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">01.
+                </span>About</li>
+            <li><span
+                    class="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">02.
+                </span>Education</li>
+            <li><span
+                    class="bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">03.
+                </span>Project</li>
+            <li><span
+                    class="bg-gradient-to-l from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">04.
+                </span>Contact</li>
+        </div>
+    </nav>
+
+    <div class="max-w-6xl mx-auto flex items-center justify-center py-10">
+        <div class="flex-1">
+            <h1>text about me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam delectus deleniti facilis itaque,
+                expedita impedit eum debitis molestiae explicabo tempore animi esse cumque quae labore consectetur
+                similique corrupti dicta sunt ducimus, sit quo quasi harum molestias. Ipsa iste debitis inventore,
+                expedita molestiae, quod repudiandae voluptas beatae, voluptate odio perspiciatis?</p>
+        </div>
+        <div class="flex-1 ">
+            <img src="{{ asset('build/assets/pixlr-image-generator-6883738872de360b5ab3e45d.png') }}"
+                class="mix-blend-multiply w-full h-auto" alt="me">
+        </div>
+    </div>
+
+</main>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+
+    <!-- tailwind server side -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+
+    {{-- live tailwind version --}}
+    {{-- <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'primary': '#5eead4',
+                        'dark': '#0f172a',
+                        'light': '#ddf8f4'
+                    },
+                    fontFamily: {
+                        'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+                    }
+                }
+            }
+        }
+    </script> --}}
+
+</head>
+
+
+<body>
+    @include('index')
+</body>
+
+</html>
