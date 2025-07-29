50768c27c7be49d0591e9cce99d290fe368773ce
Fotux <karooolys@gmail.com>
2025-07-29 21:36:49 +0300

    my skills section added with 3d cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,33 +7,160 @@
         </div>
         </div>
         <div class="flex gap-10 text-light tracking-tight ">
-            <li><span
+            <a href=""><span
                     class="bg-gradient-to-t from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">01.
-                </span>About</li>
-            <li><span
+                </span>About</a>
+            <a href=""><span
                     class="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">02.
-                </span>Education</li>
-            <li><span
+                </span>Education</a>
+            <a href=""><span
                     class="bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">03.
-                </span>Project</li>
-            <li><span
+                </span>Project</a>
+            <a href=""><span
                     class="bg-gradient-to-l from-primary to-secondary text-transparent bg-clip-text font-serif text-xl">04.
-                </span>Contact</li>
+                </span>Contact</a>
         </div>
     </nav>
 
-    <div class="max-w-6xl mx-auto flex items-center justify-center py-10">
+    <section class="max-w-5xl mx-auto flex items-center justify-center py-15">
         <div class="flex-1">
-            <h1>text about me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam delectus deleniti facilis itaque,
-                expedita impedit eum debitis molestiae explicabo tempore animi esse cumque quae labore consectetur
-                similique corrupti dicta sunt ducimus, sit quo quasi harum molestias. Ipsa iste debitis inventore,
-                expedita molestiae, quod repudiandae voluptas beatae, voluptate odio perspiciatis?</p>
+
+            <h1
+                class="from-primary to-secondary bg-gradient-to-b bg-clip-text text-transparent text-5xl tracking-wide font-serif">
+                Backend
+                Developer</h1>
+            <p class="text-dark text-lg mt-6 font-serif">Passionate backend developer with a grasp of frontend
+                technologies. I
+                enjoy
+                creating
+                clean, scalable, and
+                maintainable solutions — from APIs to full-stack applications.</p>
+            <button
+                class="border rounded-2xl px-4 py-2 mt-6 text-shadow-xl bg-dark font-bold  transition-all duration-500 hover:scale-105 ease-in-out">
+                <span
+                    class="text-light hover:bg-black hover:from-primary hover:to-secondary hover:bg-gradient-to-b hover:bg-clip-text hover:text-transparent transition-all duration-500">Contact
+                    Me</span>
+            </button>
+
+
+
+
+
         </div>
         <div class="flex-1 ">
             <img src="{{ asset('build/assets/pixlr-image-generator-6883738872de360b5ab3e45d.png') }}"
                 class="mix-blend-multiply w-full h-auto" alt="me">
         </div>
-    </div>
+    </section>
+
+    <section>
+
+        <h1 class="text-center text-5xl text-dark font-medium my-30 font-serif">My Skills</h1>
+
+        <div
+            class="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-10 justify-items-center items-stretch [perspective:800px]">
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/PHP.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">PHP
+                </h1>
+                <p class="text-light/70">1 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/js.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">JS
+                </h1>
+                <p class="text-light/70">1 year Knowledge</p>
+            </div>
+
+
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/html.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">HTML
+                </h1>
+                <p class="text-light/70">4 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/css.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">CSS
+                </h1>
+                <p class="text-light/70">4 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/mysql.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">MYSQL
+                </h1>
+                <p class="text-light/70">1 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/tailwind.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">
+                    TAILWIND</h1>
+                <p class="text-light/70">1 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/wordpress.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">
+                    WORDPRESS</h1>
+                <p class="text-light/70">1 year Knowledge</p>
+            </div>
+
+            <div
+                class="cards text-center font-bold border-2 shadow-2xl bg-dark border-light text-light rounded-2xl flex flex-col justify-between items-center h-full p-5 hover:border-secondary hover:shadow-[var(--glow-primary)] duration-100 transition-all ease">
+                <img src="{{ asset('build/assets/git.svg') }}" alt="" class="mx-auto mb-4 w-20 h-20">
+                <h1 class="text-xl bg-gradient-to-b from-primary to-secondary text-transparent bg-clip-text my-4">GIT
+                </h1>
+                <p class="text-light/70">2 year Knowledge</p>
+            </div>
+        </div>
+
+
+        {{-- script for 3d cards location --}}
+        <script>
+            const cards = document.querySelectorAll('.cards');
+
+            cards.forEach(card => {
+                card.addEventListener("mousemove", (e) => {
+                    const rect = card.getBoundingClientRect();
+
+                    // rotation
+                    const x = e.clientX - rect.left;
+                    const y = e.clientY - rect.top;
+
+                    // center position
+                    const centerX = rect.width / 2;
+                    const centerY = rect.height / 2;
+
+                    // rotations from center
+                    const rotateX = (centerX - x) / 10;
+                    const rotateY = -(centerY - y) / 10;
+
+                    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.10)`;
+                });
+            });
+
+            cards.forEach(card => {
+                card.addEventListener("mouseleave", () => {
+                    card.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+                })
+            });
+        </script>
+    </section>
+
+
+    <section>
+        <h1 class="text-center text-4xl my-30">My Projects</h1>
 
 </main>
